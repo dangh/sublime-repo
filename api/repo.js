168d9dc857d6351f7b4befa5b566fb270b1e08e3
Dang <huynhminhdang@gmail.com>
2022-07-12 16:04:14 +0700

    support multiple repos in one request

--- api/repo.js
+++ api/repo.js
@@ -1,56 +1,43 @@
-const { Octokit } = require('@octokit/rest');
-
-const GITHUB_ACCESS_TOKEN = process.env.GITHUB_ACCESS_TOKEN;
+const https = require('https');
 
 module.exports = async (req, res) => {
-  let [owner, repo, ...branch] = req.query.url.split('/');
-  branch = branch.join('/');
-  let octokit = new Octokit({ auth: GITHUB_ACCESS_TOKEN });
-  let ref;
-  if (branch) {
-    ref = (await octokit.git.getRef({ owner, repo, ref: `heads/${branch}` })).data;
-  } else {
-    let [master, main] = await Promise.all([
-      octokit.git.getRef({ owner, repo, ref: `heads/master` }).catch(err => {}),
-      octokit.git.getRef({ owner, repo, ref: `heads/main` }).catch(err => {}),
-    ]);
-    branch = master ? 'master' : 'main';
-    ref = (master || main).data;
-  }
-  let { data: commit } = await octokit.git.getCommit({ owner, repo, commit_sha: ref.object.sha });
-  let commitDate = new Date(commit.committer.date);
-  let detailsUrl = `https://github.com/${owner}/${repo}`;
-  if (!['master', 'main'].includes(branch)) detailsUrl += `/tree/${branch}`;
   res.json({
     schema_version: '3.0.0',
-    packages: [{
-      details: detailsUrl,
-      releases: [{
-        version: strftime(commitDate, '%Y.%m.%d.%H.%M.%S'),
-        url: `https://github.com/${owner}/${repo}/archive/${branch}.zip`,
-        date: strftime(commitDate, '%Y-%m-%d %H:%M:%S'),
-        sublime_text: '*'
-      }]
-    }]
+    packages: await Promise.all(
+      req.query.url.split(',').map(getPackage)
+    )
   });
 };
 
-function strftime(date, format) {
-  return format.replace(/%[a-zA-Z]/g, c => {
-    switch (c[1]) {
-      case 'Y': return date.getFullYear();
-      case 'y': return String(date.getFullYear()).slice(-2);
-      case 'm': return String(date.getMonth() + 1).padStart(2, '0');
-      case 'n': return date.getMonth() + 1;
-      case 'd': return String(date.getDate()).padStart(2, '0');
-      case 'e': return date.getDate();
-      case 'H': return String(date.getHours()).padStart(2, '0');
-      case 'I': return String(date.getHours() % 12).padStart(2, '0');
-      case 'k': return date.getHours();
-      case 'l': return date.getHours() % 12;
-      case 'M': return String(date.getMinutes()).padStart(2, '0');
-      case 'S': return String(date.getSeconds()).padStart(2, '0');
-      default: return c;
-    }
+async function getPackage(url) {
+  let [owner, repo, ...branch] = url.split('/');
+  branch = branch.join('/');
+  let commit;
+  if (branch) commit = await getJSON(`https://api.github.com/repos/${owner}/${repo}/commits/${branch}`);
+  else [commit] = await getJSON(`https://api.github.com/repos/${owner}/${repo}/commits?per_page=1`);
+  return {
+    details: `https://github.com/${owner}/${repo}`,
+    releases: [{
+      version: commit.commit.committer.date.split(/[^\d]+/).filter(Boolean).join('.'),
+      url: `https://api.github.com/repos/${owner}/${repo}/tarball/${commit.sha}`,
+      date: commit.commit.committer.date.replace('T', ' ').replace('Z', ''),
+      sublime_text: '*',
+    }],
+  };
+}
+
+function getJSON(url) {
+  return new Promise((resolve, reject) => {
+    let chunks = [];
+    let opts = { headers: { 'user-agent': '' } }
+    https
+      .get(url, opts, res => {
+        res
+          .on('data', data => chunks.push(data))
+          .once('end', () => resolve(JSON.parse(Buffer.concat(chunks).toString('utf8'))))
+          .once('error', reject)
+      })
+      .once('error', reject)
+      .end();
   });
 }
